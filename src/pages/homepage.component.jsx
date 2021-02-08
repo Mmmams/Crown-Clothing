276847dc8700a99face2ee96859a46d5899b374f
Сергей Мамыкин <bymamykin@gmail.com>
2021-02-09 01:07:16 +0300

    create new project

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './homepage.style.scss'
+
+import Directional from '../components/directional/directional.component'
+
+const HomePage = () =>{
+    return(
+    <div className='homepage'>
+        <Directional/>
+    </div>
+
+)}
+
+export default HomePage;
