3d7f5f600811df34f133ac27ee8742246c99dd05
Sergei Mamykin <bymamykin@gmail.com>
2021-03-10 13:34:29 +0300

    feat: add function adding user data in store

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
-import React from 'react'
-import {Route, Switch} from 'react-router-dom'
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component.jsx'
-import ShopPage from './pages/shop/shop.component.jsx'
-import Header from './components/header/header.component.jsx'
-import SingInAndSingUp from './pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx'
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import Header from "./components/header/header.component.jsx";
+import SingInAndSingUp from "./pages/sing-in-and-sing-up/sing-in-and-sing-up.component.jsx";
 
-import './App.css';
+import "./App.css";
 
-import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-
-
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      currentUser : null
-    }
+      currentUser: null,
+    };
   }
   unsubscribeFromAuth = null;
 
-
-  componentDidMount(){
-   this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      createUserProfileDocument(user);
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot((snapShot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              },
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
-  render(){
+  render() {
     return (
-      <div >
-      <Header currentUser={this.state.currentUser}/>
+      <div>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
-          <Route exact path='/' component={HomePage}/>
-          <Route path='/shop' component={ShopPage}/>
-          <Route path='/singin' component={SingInAndSingUp}/>
-        <HomePage />
+          <Route exact path="/" component={HomePage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route path="/singin" component={SingInAndSingUp} />
+          <HomePage />
         </Switch>
       </div>
     );
   }
-
 }
 
 export default App;
