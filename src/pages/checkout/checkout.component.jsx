8a404468d2b60f8530c7bc80ba4bea85f3f49c4a
Sergei Mamykin <bymamykin@gmail.com>
2021-03-18 15:30:53 +0300

    feat: added payment

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -8,6 +8,8 @@ import {
   selectCartTotal,
   selectCartItem,
 } from "../../redux/cart/cart.selectors";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
 const CheckoutPage = ({ cartItems, total }) => (
   <div className="checkout-page">
     <div className="checkout-header">
@@ -31,6 +33,12 @@ const CheckoutPage = ({ cartItems, total }) => (
       <CheckoutItem cartItem={item} key={item.id} />
     ))}
     <div className="total">TOTAL ${total}</div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp:01/22 - CVV:123
+    </div>
+    <StripeCheckoutButton price={total} />
   </div>
 );
 
