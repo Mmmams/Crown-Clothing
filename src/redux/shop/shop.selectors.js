9e82af305fbba720fd0cbfeeffaee94c0f1a19ed
Sergei Mamykin <bymamykin@gmail.com>
2021-03-18 13:41:48 +0300

    fix: changed my SHOP_DATA from array to hash-table, for data normalization feat: created new selector for handle hash-table in array for collection-preview component

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,13 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_MAP_ID = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -16,8 +8,15 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_MAP_ID[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[collectionUrlParam]
   );
+
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  (collections) => {
+    console.log(collections);
+    return Object.keys(collections).map((key) => collections[key]);
+  }
+);
