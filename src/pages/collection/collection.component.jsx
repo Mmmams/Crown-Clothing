c3929399cb61e1d1d9bc5b2454f9c2d57da25edf
Sergei Mamykin <bymamykin@gmail.com>
2021-03-18 11:15:56 +0300

    fix: resolved problem with "undefined" - collection in CollectionPage

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -9,14 +9,14 @@ import "./collection.style.scss";
 const CollectionPage = ({ collection }) => {
   console.log(collection);
   return (
-    <div className="category">
+    <div className="collection-page">
       <h2>COLLECTION PAGE</h2>
     </div>
   );
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.path.collectionId)(state),
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import CollectionOverview from "../../components/collection-overview/collection-overview.component";
-import Collection from "../../pages/collection/collection.component";
+import CollectionPage from "../../pages/collection/collection.component";
+
 const ShopPage = ({ match }) => {
   return (
     <div className="shop-page">
       <Route exact path={`${match.path}`} component={CollectionOverview} />
-      <Route path={`${match.path}/:categoryId`} component={Collection} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
   );
 };
